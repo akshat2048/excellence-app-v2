8a8637ea6917cbf63bb1fdf236a322965af94d99
akshat2048 <akshatchannashetti@gmail.com>
2021-02-23 11:56:52 -0600

    init

--- components/TabBarController/ScheduleScreen/CalendarSchedulerScreen.js
+++ components/TabBarController/ScheduleScreen/CalendarSchedulerScreen.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-
+import { Calendar } from 'react-native-calendars'
 
 export default class CalendarSchedulerScreen extends Component {
     render() {
         return(
             <View>
-                <Text>Ni</Text>
+                <Calendar style={styles.calendar} theme={
+                    {
+                        calendarBackground: '#000000',
+                        dayTextColor: 'white'
+                    }
+                }/>
             </View>
         )
+    }
+
+    onDayPress(day) {
+
     }
 }
 
 const styles = StyleSheet.create({
-
+    
+    month: {
+        color: '#FFFFFF'
+    }
 })
